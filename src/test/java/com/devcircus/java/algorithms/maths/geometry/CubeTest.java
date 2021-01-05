c61bfacd4c9c24e251d13c7d02d7986f45a2eebc
Adrian Novegil <adrian.novegil@gmail.com>
2021-01-05 17:15:56 +0100

    Removing no necessary test code

--- src/test/java/com/devcircus/java/algorithms/maths/geometry/CubeTest.java
+++ src/test/java/com/devcircus/java/algorithms/maths/geometry/CubeTest.java
@@ -1,9 +1,5 @@
 package com.devcircus.java.algorithms.maths.geometry;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -11,25 +7,6 @@ import org.junit.Test;
  * @author Adrian Novegil <adrian.novegil@gmail.com>
  */
 public class CubeTest {
-
-    public CubeTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
 
     @Test
     public void testArea() {
